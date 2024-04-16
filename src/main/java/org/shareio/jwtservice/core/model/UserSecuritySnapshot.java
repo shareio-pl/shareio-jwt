6b1260b9b4b64fd6055feaa994b226a648a72a33
Tomunek <kowalczyk.tomek11@gmail.com>
2024-04-16 19:17:49 +0200

    Added real account type to user

--- src/main/java/org/shareio/jwtservice/core/model/UserSecuritySnapshot.java
+++ src/main/java/org/shareio/jwtservice/core/model/UserSecuritySnapshot.java
@@ -1,20 +1,19 @@
 package org.shareio.jwtservice.core.model;
 
+import org.shareio.jwtservice.core.model.vo.AccountType;
 import org.shareio.jwtservice.core.model.vo.JWTToken;
-import org.shareio.jwtservice.core.model.vo.Role;
 import org.shareio.jwtservice.core.model.vo.UserId;
 
-import java.util.List;
-
-public record UserSecuritySnapshot (
+public record UserSecuritySnapshot(
         UserId userId,
         String email,
         String password,
-        Role role,
+        AccountType accountType,
         JWTToken jwtToken
 
-){
+) {
     public UserSecuritySnapshot(UserSecurity userSecurity) {
-        this(userSecurity.getUserId(), userSecurity.getEmail(), userSecurity.getPassword(), userSecurity.getRole(), userSecurity.getJwtToken());
+        this(userSecurity.getUserId(), userSecurity.getEmail(), userSecurity.getPassword(),
+                userSecurity.getAccountType(), userSecurity.getJwtToken());
     }
 }
--- src/main/java/org/shareio/jwtservice/core/usecases/service/GenerateJWTTokenUseCaseService.java
+++ src/main/java/org/shareio/jwtservice/core/usecases/service/GenerateJWTTokenUseCaseService.java
@@ -12,8 +12,6 @@ import org.shareio.jwtservice.core.usecases.port.dto.GetTokenResponseDto;
 import org.shareio.jwtservice.core.usecases.port.dto.UserSecurityGetDto;
 import org.shareio.jwtservice.core.usecases.port.in.GenerateJWTTokenUseCaseInterface;
 import org.shareio.jwtservice.core.usecases.port.out.GetUserSecurityDaoInterface;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -25,32 +23,35 @@ import java.util.Optional;
 @Service
 @AllArgsConstructor
 public class GenerateJWTTokenUseCaseService implements GenerateJWTTokenUseCaseInterface {
-
     PropertySource propertySource;
     GetUserSecurityDaoInterface getUserSecurityDaoInterface;
-    private static final BCryptPasswordEncoder passwordEncorder = new BCryptPasswordEncoder();
+    private static final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     @Override
     public GetTokenResponseDto generateJWTToken(GetTokenRequestDto getTokenRequestDto) {
-        String secret = propertySource.getSecret();
         Optional<UserSecurityGetDto> userSecurityGetDto = getUserSecurityDaoInterface.getUserDto(getTokenRequestDto.getEmail());
-        if(userSecurityGetDto.isPresent()){
-            UserSecurity userSecurity = userSecurityGetDto.map(UserSecurity::fromDto).get();
-            if(verifyPasswordMatch(getTokenRequestDto.getPassword(), userSecurityGetDto.get().password())){
-                UserSecuritySnapshot userSecuritySnapshot = userSecurity.toSnapshot();
-                return new GetTokenResponseDto(Jwts.builder()
-                        .claim("id",userSecuritySnapshot.userId().getId())
-                        .claim("role","user")
-                        .setSubject("SHAREIO")
-                        .setIssuedAt(Date.from(Instant.now()))
-                        .setExpiration(Date.from(Instant.now().plus(10, ChronoUnit.MINUTES)))
-                        .signWith(Keys.hmacShaKeyFor(secret.getBytes()),SignatureAlgorithm.HS256).compact());
-            }
+        if (userSecurityGetDto.isEmpty()) {
+            return null;
         }
+
+        if (verifyPasswordMatch(getTokenRequestDto.getPassword(), userSecurityGetDto.get().password())) {
+            // TODO: set last login date
+            UserSecuritySnapshot userSecuritySnapshot = userSecurityGetDto
+                    .map(UserSecurity::fromDto).get().toSnapshot();
+            return new GetTokenResponseDto(Jwts.builder()
+                    .claim("id", userSecuritySnapshot.userId().getId())
+                    .claim("role", userSecuritySnapshot.accountType())
+                    .setSubject("SHAREIO")
+                    .setIssuedAt(Date.from(Instant.now()))
+                    .setExpiration(Date.from(Instant.now().plus(propertySource.getDuration(), ChronoUnit.MINUTES)))
+                    .signWith(Keys.hmacShaKeyFor(propertySource.getSecret().getBytes()), SignatureAlgorithm.HS256)
+                    .compact());
+        }
+
         return null;
     }
 
     private Boolean verifyPasswordMatch(String providedPassword, String dbPassword) {
-        return passwordEncorder.matches(providedPassword, dbPassword);
+        return passwordEncoder.matches(providedPassword, dbPassword);
     }
 }
